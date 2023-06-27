c43c93d647e76f91adaac3727d7690cff78bdb37
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-27 10:46:59 +0200

    WIP

--- script.js
+++ script.js
@@ -30,20 +30,60 @@ function checkKey(e) {
 
 document.onclick = createRock;
 
-function moveRock(rock) {
-    document.getElementById(rock).className = "btn btn-warning";
+// function moveRock(rock) {
+//     document.getElementById(rock).className = "btn btn-warning";
+//     if (rock[0] < 5) {
+//         rock = ++rock[0] + rock[1];
+//         alert(rock);
+//         document.getElementById(rock).className = "btn btn-danger";
+//         // setTimeout(moveRock(rock), 3000);
+//     } else {
+//         clearInterval(interval);
+//     }
+// }
+
+let rock = "0" + Math.floor(Math.random() * 7);
+
+function moveRock() {
+    document.getElementById(rock).className = "btn btn-danger";
     if (rock[0] < 5) {
+        document.getElementById(rock).className = "btn btn-warning";
         rock = ++rock[0] + rock[1];
-        alert(rock);
         document.getElementById(rock).className = "btn btn-danger";
-        // setTimeout(moveRock(rock), 3000);
     } else {
-        clearInterval(interval);
+        document.getElementById(rock).className = "btn btn-warning";
+        rock = "0" + Math.floor(Math.random() * 7);
+        document.getElementById(rock).className = "btn btn-danger";
+
     }
 }
 
-function createRock {
-    let rock = "0" + Math.floor(Math.random() * 7);
-    document.getElementById(rock).className = "btn btn-danger";
-    let interval = setInterval(moveRock(), 100, rock);
-}
+let intervalID = window.setInterval(moveRock, 1000);
+
+// function myCallback() {
+//  // Your code here
+// }
+
+
+// sleep(500).then(() => {
+//     // Do something after the sleep!
+//     rock = ++rock[0] + rock[1];
+// });
+
+// function sleep(time) {
+//     return new Promise((resolve) => setTimeout(resolve, time));
+//   }
+  
+  // Usage!
+
+
+// const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+// async function repeatedGreetingsLoop() {
+//   for (let i = 1; i <= 5; i++) {
+//     await sleepNow(1000)
+//     console.log(`Hello #${i}`)
+//   }
+// }
+
+// repeatedGreetingsLoop()
