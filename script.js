6bbab157b2dd3419c2133a600baed545e0a8dcc9
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-27 14:50:51 +0200

    First Release

--- script.js
+++ script.js
@@ -19,16 +19,18 @@ const directions = {
     '68': 'right'
 };
 
+const lines = 5, columns = 6;
+
 function movePlayer(e) {
     e = e || window.event;
     document.getElementById(playerId).className = "btn btn-warning";
     if (directions[e.keyCode] === 'up' && playerId[0] > 0) {
         playerId = --playerId[0] + playerId[1];
-    } else if (directions[e.keyCode] === 'down' && playerId[0] < 5) {
+    } else if (directions[e.keyCode] === 'down' && playerId[0] < lines) {
         playerId = ++playerId[0] + playerId[1];
     } else if (directions[e.keyCode] === 'left' && playerId[1] > 0) {
         playerId = playerId[0] + --playerId[1];
-    } else if (directions[e.keyCode] === 'right' && playerId[1] < 6) {
+    } else if (directions[e.keyCode] === 'right' && playerId[1] < columns) {
         playerId = playerId[0] + ++playerId[1];
     }
     if (document.getElementById(playerId).className === "btn btn-danger") {
