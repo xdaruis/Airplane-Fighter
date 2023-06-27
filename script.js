ca9a8abc449d074cf69b1e7f3371e3deb062f49c
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-27 14:32:08 +0200

    First Release

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ document.getElementById(rock).className = "btn btn-danger";
 let rockSpeed = window.setInterval(dropRock, 100);
 let scoreValue = 0;
 
+const directions = {
+    '38': 'up',
+    '87': 'up',
+    '40': 'down',
+    '83': 'down',
+    '37': 'left',
+    '65': 'left',
+    '39': 'right',
+    '68': 'right'
+};
+
 function movePlayer(e) {
     e = e || window.event;
     document.getElementById(playerId).className = "btn btn-warning";
-    if ((e.keyCode == '38' || e.keyCode == '87') && playerId[0] > 0) { // up
+    if (directions[e.keyCode] === 'up' && playerId[0] > 0) {
         playerId = --playerId[0] + playerId[1];
-    } else if ((e.keyCode == '40' || e.keyCode == '83') && playerId[0] < 5) { // down
+    } else if (directions[e.keyCode] === 'down' && playerId[0] < 5) {
         playerId = ++playerId[0] + playerId[1];
-    } else if ((e.keyCode == '37' || e.keyCode == '65') && playerId[1] > 0) { // left
+    } else if (directions[e.keyCode] === 'left' && playerId[1] > 0) {
         playerId = playerId[0] + --playerId[1];
-    } else if ((e.keyCode == '39' || e.keyCode == '68') && playerId[1] < 6) { // right
+    } else if (directions[e.keyCode] === 'right' && playerId[1] < 6) {
         playerId = playerId[0] + ++playerId[1];
     }
     if (document.getElementById(playerId).className === "btn btn-danger") {
